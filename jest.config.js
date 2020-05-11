2976538e5f9a35ea3b9fda1bf6ea4a6c83027b8f
fanluping <fanluping@bytedance.com>
2020-05-11 23:32:18 +0800

    feat: add jest.config.ci.js

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,10 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
-  collectCoverage: true,
-  reporters: ["default", "jest-junit"],
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
+  reporters: ["default"],
+
+  collectCoverage: false,
+
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
   globals: {
@@ -17,9 +16,9 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
-    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js"
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
   },
-  testMatch: ['<rootDir>/**/*/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
+  testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
   transform: {
     "^.+unit\\.(js|jsx)$": "babel-jest",
     '^.+\\.(ts|tsx)$': 'ts-jest',
