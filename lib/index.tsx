eaff3ba79e81d32b630a2009d11e2bf26669c884
fanluping <fanluping@bytedance.com>
2020-05-11 21:37:51 +0800

    fix: change package name in case someone used my package

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,14 +1,2 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import Button from './button'
-import Icon from './icon/icon'
-
-const fn: React.MouseEventHandler<SVGUseElement | SVGElement> = e => {
-  console.log(e.target)
-}
-
-ReactDOM.render(
-<div>
-  <Icon icon="bytedance" onClick={fn}/>
-</div>, document.getElementById('root'))
+export {default as Icon} from './icon/icon'
 
