2ae2c09a2f0e7050f1d877275915fd954e3fcfcb
fanluping <fanluping@bytedance.com>
2020-05-12 09:39:52 +0800

    feat: add example.tsx and example.html

---
+++ example.tsx
@@ -0,0 +1,10 @@
+import {Icon} from './lib/index'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render((
+  <div>
+    <Icon icon="bytedance"/>
+  </div>
+), document.getElementById('root'))
