a0925efec52e8bd5c150a163284a516af7759cfe
fanluping <fanluping@bytedance.com>
2020-05-12 09:54:29 +0800

    feat: add IconExample

--- example.tsx
+++ example.tsx
@@ -1,10 +1,32 @@
-import {Icon} from './lib/index'
+import { IconExample } from './lib/icon/Icon.example'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router, Link, Route } from 'react-router-dom'
 
-ReactDOM.render((
-  <div>
-    <Icon icon="bytedance"/>
-  </div>
-), document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <div>
+      <header>
+        <div className="logo">Freact</div>
+      </header>
+      <div>
+        <aside>
+          <h2>组件</h2>
+          <ul>
+            <li>
+              <Link to="/icon">Icon</Link>
+            </li>
+            <li>
+              <Link to="/button">Button</Link>
+            </li>
+          </ul>
+        </aside>
+        <main>
+          <Route path="/icon" component={IconExample}></Route>
+        </main>
+      </div>
+    </div>
+  </Router>,
+  document.getElementById('root'),
+)
---
+++ lib/icon/Icon.example.tsx
@@ -0,0 +1,12 @@
+import Icon from './icon'
+import React from 'react'
+
+export const IconExample: React.FC = () => {
+  return (
+    <div>
+      <Icon icon="alipay" />
+      <Icon icon="qq" />
+      <Icon icon="bytedance" />
+    </div>
+  )
+}
